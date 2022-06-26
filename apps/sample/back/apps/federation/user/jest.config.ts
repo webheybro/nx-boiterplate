099d816dbf761dbc8253d431e78c1c70f5e06991
Brault Mickael <web.heybro@gmail.com>
2022-06-26 11:59:52 +0200

    tsconfig

---
+++ apps/sample/back/apps/federation/user/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'sample-back-apps-federation-user',
+  preset: '../../../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../../../coverage/apps/sample/back/apps/federation/user',
+};
