b9879f0f14c127539c4ce8f71635195af44cd4ab
Brault Mickael <web.heybro@gmail.com>
2022-06-26 16:26:52 +0200

    husky format

--- tools/eslint/eslint-next.js
+++ tools/eslint/eslint-next.js
@@ -2,11 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['.eslintrc.js'],
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
